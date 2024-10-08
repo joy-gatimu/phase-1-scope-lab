2b1e7cd88a4e659bbb44169cbd2da4d47dabd09d
Joy <joeyygatimu@gmail.com>
2024-10-08 11:30:28 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+window.customerName = 'bob';
+window.bestCustomer = undefined;
+const leastFavoriteCustomer = 'someone';
+
+function upperCaseCustomerName() {
+  window.customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  window.bestCustomer = newBestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+  throw new Error('Assignment to constant variable.');
+}
